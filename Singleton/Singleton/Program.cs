8b92005b46f586f42d36ed14ff3968b3ce246175
Kadir Mutlu <kadirmutluu@gmail.com>
2016-06-08 17:52:06 +0300

    Singleton tasarım kalıbı eklendi.

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TekNesne n1 = TekNesne.ornekGetir();
+            TekNesne n2 = TekNesne.ornekGetir();
+            TekNesne n3 = TekNesne.ornekGetir();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton/Singleton/TekNesne.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+    public class TekNesne
+    {
+        private static TekNesne ornek;
+
+        private TekNesne(){}
+        public static TekNesne ornekGetir()
+        {
+            if (ornek == null)
+            {
+                ornek = new TekNesne();
+                Console.WriteLine("Yeni Örnek nesne üretildi.");
+            }
+            else
+                Console.WriteLine("Önceden yaratılmış örnek geri döndürüldü.");
+
+            return ornek;
+        }
+
+    }
+}
